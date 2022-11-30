a748b76aedcca883e74141bbd85cde7410722634
rogseo <rogseo@gmail.com>
2022-11-29 21:59:55 -0600

    init

---
+++ yeon-portfolio/src/index.js
@@ -0,0 +1,14 @@
+
+import ReactDOM from 'react-dom';
+import React from 'react'
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
