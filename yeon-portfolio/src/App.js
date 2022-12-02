9cdfbee051daafe86e427631015e1f261c233c08
rogseo <rogseo@gmail.com>
2022-12-02 02:03:59 -0600

    App.js edited : header and footer return

--- yeon-portfolio/src/App.js
+++ yeon-portfolio/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main className="text-gray-400 bg-gray body-font">
+      <Header/>
+      <Footer/>
+    </main>
+  )
+           
 
+};
 export default App;
+
+
