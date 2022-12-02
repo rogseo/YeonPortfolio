1877f27f1d98d9bf983cdbe7a0a952f302aaf8e4
rogseo <rogseo@gmail.com>
2022-12-02 02:13:50 -0600

    project data load

---
+++ yeon-portfolio/src/data.js
@@ -0,0 +1,51 @@
+export const projects = [
+    {
+      title: "tuneTalk",
+      subtitle: "MERN Stack",
+      description:
+      "Node/Mysql/express-session/Sequelize/Express-handlebars/Bootstrap",
+      image: "../../images/tuneTalk demo_edit.gif",
+      link: "https://reactbootcamp.com",
+      repo : "https://github.com/drewsparker/tuneTalk"
+    },
+    {
+      title: "TechBlog",
+      subtitle: "React and Python",
+      description: "Node/Mysql/Sequelize/Express-handlebars/Bootstrap/API",
+      image: "./assets/image/challenge 14 demo.gif",
+      link: "https://tech-blogg-story.herokuapp.com/",
+      repo: "https://github.com/rogseo/techBlog"
+    },
+    {
+      title: "Movie&more",
+      subtitle: "React and Firebase",
+      description: "HTML/CSS/Javascript/API/Foundation",
+      image: "./project-3.gif",
+      link: "https://https://sarahthoorens.github.io/movies-and-more/.com",
+      repo: "https://github.com/sarahthoorens/movies-and-more"
+    },
+    {
+      title: "Weather Dashboard",
+      subtitle: "React Hooks",
+      description:"HTML/CSS/Javascript/API/Bootstrap",
+      image: "./project-4.gif",
+      link: "https://rogseo.github.io/weatherDashboard/",
+      repo:"https://github.com/rogseo/weatherDashboard"
+    },
+    {
+      title: "Password Generator",
+      subtitle: "React Hooks",
+      description:"HTML/CSS/Javascript",
+      image: "./project-4.gif",
+      link: "https://rogseo.github.io/passwordGenerator/",
+      repo:"https://github.com/rogseo/passwordGenerator"
+    },
+    {
+      title: "Timer Quiz",
+      subtitle: "React Hooks",
+      description:"HTML/CSS/Javascript",
+      image: "./project-4.gif",
+      link: "https://github.com/rogseo/codeQuiz",
+      repo:"https://rogseo.github.io/codeQuiz/"
+    },
+  ];
