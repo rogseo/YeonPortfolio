ae5e0323bc8142aa1891fcb998cd2d07df8956fd
rogseo <rogseo@gmail.com>
2022-12-04 10:56:13 -0600

    change file name

--- yeon-portfolio/src/data.js
+++ yeon-portfolio/src/data.js
@@ -50,7 +50,7 @@ export const projects = [
       title: "Timer Quiz",
       subtitle: "React Hooks",
       description:"This App will ask 5 questions with 4 options of answers. The time is given for 75s and score is equal to the time left. When you answer incorrectly, time is subtracted 10s from the clock. When you finish all questions, you are presented with a score and you can save your initial and score. You can gauge your progress compared to my peers.",
-      image: 'Code Quiz.GIF',
+      image: 'Code Quiz.gif',
       link: "https://github.com/rogseo/codeQuiz",
       repo:"https://rogseo.github.io/codeQuiz/",
       tech:['HTML','Javascript','CSS','Bootstrap']
