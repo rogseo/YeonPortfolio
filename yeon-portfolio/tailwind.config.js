384909117cc2f58aa6fe541aa36f9342be549ef4
rogseo <rogseo@gmail.com>
2022-12-03 20:47:52 -0600

    add google font

--- yeon-portfolio/tailwind.config.js
+++ yeon-portfolio/tailwind.config.js
@@ -1,14 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+
+
 module.exports = {
-   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      'Concert': ['"Concert One"', 'cursive'],
+      'serif': ['"ui-serif"', 'Georgia'],
+      'mono': ['"Space Mono"', 'monospace'],
+      'Neuton': ['"Neuton"', 'serif'],
+      'Noto': ['"Noto Serif Malayalam"', 'serif'],
+    },
     extend: {
       colors: {
         'black': '#0B0C10',
-        'gray' : '#1F2833',
+        'gray': '#1F2833',
         'light-gray': '#C5C6C7',
         'light-green': '#66FCF1',
-        'green':'#45A29E',
+        'green': '#45A29E',
       },
     },
   },
